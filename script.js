06b86d68a5ad01b3fa0bc9ee8201cf90395739ca
Dharmesh Mahajan <dharmeshmahajan2@gmail.com>
2023-05-25 11:54:18 +0530

    Make required UI changes

--- script.js
+++ script.js
@@ -1,54 +1,86 @@
 let player = 0, computer = 0;
+let playerSelection;
 
-function playRound(playerSelection, computerSelection){
-    if(!(playerSelection === computerSelection)){
-        if(playerSelection === "rock"){
-            if(computerSelection === "paper") 
-                computer++;        
+result = document.getElementById("results");
+pScore = document.getElementById("playerScore");
+cScore = document.getElementById("computerScore");
 
-            else
-                player++;        
-        }
 
-        else if(playerSelection === "paper"){
-            if(computerSelection === "scissors")
-                computer++;       
-            
-            else
-                player++;       
-        }
+pScore.innerHTML = "Player: " + player;
+cScore.innerHTML = "Computer: " + computer;
 
-        else{
-            if(computerSelection === "rock")
-                computer++;      
-            
-            else
-                player++;        
-        }
+function reply_click(){
+    playerSelection = event.target.id;
+
+    function getComputerChoice(){
+        let x = Math.floor(Math.random()*10);
+        if(x%3 == 0)
+            return "rock";
+        else if(x%3 == 1)
+            return "paper";
+        else
+            return "scissors";
     }
     
-}
-
-function game(){
-    for(let i = 0;i<5;i++){
-        let playerSelection = prompt("Enter choice: ");
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+    function playRound(playerSelection, computerSelection){
+        if(!(playerSelection === computerSelection)){
+            if(playerSelection === "rock"){
+                if(computerSelection === "paper") 
+                    computer++;        
+    
+                else
+                    player++;        
+            }
+    
+            else if(playerSelection === "paper"){
+                if(computerSelection === "scissors")
+                    computer++;       
+                
+                else
+                    player++;       
+            }
+    
+            else{
+                if(computerSelection === "rock")
+                    computer++;      
+                
+                else
+                    player++;        
+            }
+        }
+        
     }
 
-    if(player > computer) console.log("player");
-    else if(player == computer) console.log("draw");
-    else return console.log("computer");
-}
+    function game(){
+        // for(let i = 0;i<5;i++){
+        //     let playerSelection = prompt("Enter choice: ");
+        //     let computerSelection = getComputerChoice();
+        //     playRound(playerSelection, computerSelection);
+        // }
+        
+        let computerSelection = getComputerChoice();
+    
+        console.log(playerSelection);
+        console.log(computerSelection);
+    
+        console.log(player);
+        console.log(computer);
+    
+        playRound(playerSelection, computerSelection);
+        if(player>=5 && computer<5){
+            console.log("player");
 
-function getComputerChoice(){
-    let x = Math.floor(Math.random()*10);
-    if(x%3 == 0)
-        return "rock";
-    else if(x%3 == 1)
-        return "paper";
-    else
-        return "scissors";
-}
+            result.innerHTML = "Player wins!";
 
-game();
+        } 
+        else if(player<5 && computer>=5){
+            console.log("computer");
+
+            result.innerHTML = "Computer wins!";
+
+        }
+    }
+    game();
+    pScore.innerHTML = "Player: " + player;
+    cScore.innerHTML = "Computer: " + computer;
+}
